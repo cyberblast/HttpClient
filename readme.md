3635de2680263bd013d59ba05093c36ab3234dd0
cyberblast <git@cyberblast.org>
2019-12-16 00:06:00 +0100

    iniial

---
+++ readme.md
@@ -0,0 +1,12 @@
+# cyberblast HttpClient
+
+A simple HttpClient for node.js
+
+[![Build Status](https://travis-ci.com/cyberblast/httpclient.svg?branch=dev)](https://travis-ci.com/cyberblast/httpclient)
+[![npm version](https://badge.fury.io/js/%40cyberblast%2Fhttpclient.svg)](https://badge.fury.io/js/%40cyberblast%2Fhttpclient)
+
+## Legal
+
+Please take note of files [LICENSE](https://raw.githubusercontent.com/cyberblast/httpclient/master/LICENSE) and [CONTRIBUTING](https://raw.githubusercontent.com/cyberblast/httpclient/master/CONTRIBUTING).
+
+This is an experimental piece of code. This is NOT a production ready web server. Use on your own risk.
---
+++ src/httpClient.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const HttpClient = {};
+module.exports = HttpClient;
+
+HttpClient.request = function(args) {
+  return new Promise((resolve, reject) => {
+    if (args == null) {
+      reject(new Error("HttpClient request requires an args parameter!"));
+    }
+    const result = {
+      headers: null,
+      response: ''
+    }
+    const onError = function(e) {
+      reject(e);
+    }
+    const onData = function(chunk) {
+      result.response += chunk;
+    };
+    const onCompleted = function() {
+      resolve(result);
+    }
+    const onResponse = function(incomingMessage) {
+      result.headers = incomingMessage.headers;
+      incomingMessage.on('data', onData);
+      incomingMessage.on('end', onCompleted);
+    };
+    const options = {
+      method: args.method || args.body ? 'POST' : 'GET'
+    };
+    if (args.options !== undefined) {
+      Object.assign(options, args.options);
+    };
+    const clientRequest = http.request(args.url, options, onResponse);
+    clientRequest.on('error', onError);
+    if (args.body !== undefined) {
+      clientRequest.write(args.body);
+    }
+    clientRequest.end();
+  });
+}
+
+HttpClient.get = async function(url) {
+  return await HttpClient.request({ url });
+}
+HttpClient.post = async function(url, body) {
+  return await HttpClient.request({ url, body });
+}
